38451962c3642c3d7c8ae6ba9167105b5ed803e7
IronTailed <yuw863@gmail.com>
2020-06-02 10:14:51 -0400

    Add files via upload

--- Mystical/classespkg/Enemies.py
+++ Mystical/classespkg/Enemies.py
@@ -6,25 +6,25 @@ class Bandit:
         self.name = 'Bandit'
         self.damage = 2
         self.hp = 4
-        self.loot = random.randint(1,3)
+        self.loot = random.randint(2,3)
 
 class Drunk:
     def __init__(self):
         self.name = 'Drunk'
         self.damage = 1
-        self.hp = 5
-        self.loot = random.randint(1,3)
+        self.hp = 4
+        self.loot = random.randint(2,3)
 
 class Troll: 
     def __init__(self):
         self.name = 'Troll'
-        self.damage = 4
+        self.damage = 3
         self.hp = 8
-        self.loot = random.randint(2,4)
+        self.loot = random.randint(3,6)
 
 class Lich:
     def __init__(self):
         self.name = 'Lich'
-        self.damage = 6
+        self.damage = 4
         self.hp = 2
-        self.loot = random.randint(3,4)
+        self.loot = random.randint(3,7)
